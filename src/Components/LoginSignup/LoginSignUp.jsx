4ac7a63035ca00ce73c655abab69db0d34f27bcb
gowtham0898 <gowthams.0898@gmail.com>
2024-09-17 23:59:58 +0530

    console removed

--- src/Components/LoginSignup/LoginSignUp.jsx
+++ src/Components/LoginSignup/LoginSignUp.jsx
@@ -9,7 +9,6 @@ import { useState } from 'react';
 
 export const LoginSignUp = () => {
     const [action, setAction] = useState('Sign Up');
-console.log(123);
   return (
     <div className="container">
         <div className='header'>
